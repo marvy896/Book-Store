985f3d691b11a0448589f3041f331a86c98b7b84
marvy896 <onyex896@gmail.com>
2022-11-09 10:12:03 +0100

    bookApp change

--- App.js
+++ App.js
@@ -1,34 +1,56 @@
-import React, { useEffect, useState } from 'react'
-import { ActivityIndicator, FlatList, Text, View } from 'react-native'
+import React, { useEffect, useState } from "react";
+import { ActivityIndicator, FlatList, Text, View } from "react-native";
 
 export default function App() {
-  const [isLoading, setLoading] = useState(true)
-  const [data, setData] = useState([])  
- 
+  const [isLoading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
+
   useEffect(() => {
-    fetch('https://fudap-books-api.herokuapp.com/books/').then((response) => response.json())
-    .then((json) => setData(json))
-    .catch((error) => console.error(error))
-    .finally(() => setLoading(false));
-}, []);
+    fetch("https://fudap-books-api.herokuapp.com/books/")
+      .then((response) => response.json())
+      .then((json) => setData(json))
+      .catch((error) => console.error(error))
+      .finally(() => setLoading(false));
+  }, []);
 
   return (
     <View style={{ flex: 1, padding: 24 }}>
-      { isLoading ?  (
-          <ActivityIndicator testID= "loading" accessibilityLabel= "App is loading books" />
-      ): ( <View style={{ flex: 1, flexDirection: 'column', justifyContent:  'space-between'}}>
-      <Text style={{ fontSize: 18, color: 'green', textAlign: 'center'}}>{data.title}</Text>
-      <Text style={{ fontSize: 14, color: 'green', textAlign: 'center', paddingBottom: 10}}>Articles:</Text>
-      <FlatList
-        data={data.articles}
-        keyExtractor={({ id }, index) => id}
-        renderItem={({ item }) => (
-          <Text>{item.id + '. ' + item.title}</Text>
-        )}
-      />
+      {isLoading ? (
+        <ActivityIndicator
+          testID="loading"
+          accessibilityLabel="App is loading books"
+        />
+      ) : (
+        <View
+          style={{
+            flex: 1,
+            flexDirection: "column",
+            justifyContent: "space-between",
+          }}
+        >
+          <Text style={{ fontSize: 18, color: "green", textAlign: "center" }}>
+            {data.title}
+          </Text>
+          <Text
+            style={{
+              fontSize: 14,
+              color: "green",
+              textAlign: "center",
+              paddingBottom: 10,
+            }}
+          >
+            Articles:
+          </Text>
+          <FlatList
+            accessibilityLabel="books"
+            data={data.articles}
+            keyExtractor={({ id }, index) => id}
+            renderItem={({ item }) => (
+              <Text testID="book">{item.id + ". " + item.title}</Text>
+            )}
+          />
+        </View>
+      )}
     </View>
-  )}
-</View>
-);
-};
-
+  );
+}
