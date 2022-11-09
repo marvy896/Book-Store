877d1afda50d4ca48d8220f84aecf112def3dfc6
marvy896 <onyex896@gmail.com>
2022-11-09 09:25:55 +0100

    The code for the bookStore

--- App.js
+++ App.js
@@ -5,10 +5,30 @@ export default function App() {
   const [isLoading, setLoading] = useState(true)
   const [data, setData] = useState([])  
  
+  useEffect(() => {
+    fetch('https://fudap-books-api.herokuapp.com/books/').then((response) => response.json())
+    .then((json) => setData(json))
+    .catch((error) => console.error(error))
+    .finally(() => setLoading(false));
+}, []);
 
   return (
     <View style={{ flex: 1, padding: 24 }}>
-    </View>)
- 
-}
+      { isLoading ?  (
+          <ActivityIndicator testID= "loading" accessibilityLabel= "App is loading books" />
+      ): ( <View style={{ flex: 1, flexDirection: 'column', justifyContent:  'space-between'}}>
+      <Text style={{ fontSize: 18, color: 'green', textAlign: 'center'}}>{data.title}</Text>
+      <Text style={{ fontSize: 14, color: 'green', textAlign: 'center', paddingBottom: 10}}>Articles:</Text>
+      <FlatList
+        data={data.articles}
+        keyExtractor={({ id }, index) => id}
+        renderItem={({ item }) => (
+          <Text>{item.id + '. ' + item.title}</Text>
+        )}
+      />
+    </View>
+  )}
+</View>
+);
+};
 
